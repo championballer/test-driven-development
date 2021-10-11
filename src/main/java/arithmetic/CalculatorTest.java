d7f537e2f927db1ca97c9556c1155778da2b9dc0
Shiv Kumar <skumar8@atlassian.com>
2021-10-11 16:35:56 +0530

    Added Calculator tests and class

---
+++ src/main/java/arithmetic/CalculatorTest.java
@@ -0,0 +1,21 @@
+package arithmetic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+
+    @Test
+    void multiply() {
+        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+                () -> assertEquals(-4, Calculator.multiply(2, -2)),
+                () -> assertEquals(4, Calculator.multiply(-2, -2)),
+                () -> assertEquals(0, Calculator.multiply(1, 0)));
+    }
+}
